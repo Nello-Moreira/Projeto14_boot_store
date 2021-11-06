ffd4b900c373bc5020d233c4510fbb72b4069cb1
Nello-Moreira <g.moreira1@outlook.com>
2021-11-05 21:21:43 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import server from './server.js';
+
+const port = process.env.PORT;
+
+server.listen(port, () => {
+	console.log(`Server listening on port ${port}`);
+	console.log(`Server running at ${process.env.NODE_ENV} mode`);
+});
